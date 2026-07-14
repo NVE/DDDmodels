04082a43335e06e3f4ce1f732f80cdb793ecd999
Emiliano Gelati <28786167+emiliano-gelati@users.noreply.github.com>
2026-07-14 16:56:31 +0200

    LayerEvap.jl optimised for performance

--- DDDFunctions/LayerEvap.jl
+++ DDDFunctions/LayerEvap.jl
@@ -14,72 +14,54 @@ function LayerEvap!(Layers, nodaysvector, ea_S, layerUH, NoL)
   #nodaysvector = [2 3 4 5]
   #ea_S = 4.5
   #layerUH = [0.6 0.4 0.0 0.0 0.0; 0.4 0.35 0.25 0.0 0.0; 0.35 0.25 0.25 0.15 0.0; 0.3 0.25 0.2 0.15 0.1]
+
+  (ea_S == 0) && return 0.0 # return zero evaporation if input ea_S is zero
    
   total_layers_last = sum(Layers)
   
   #Below are the states (in mm) for each saturation level
   redea = ea_S
-   #println(total_layers_last)
-   #println(sum(Layers))
 
   
-  for j in 1:NoL # 1 is the top(fastest) Layer NoL is the bottom layer 
+  @inbounds for j in 1:NoL # 1 is the top(fastest) Layer NoL is the bottom layer 
 
-    if redea > 0
-
-      if sum(Layers) > 0
+        (redea <= 0) && break # exit the layers loop if there is no more water to evaporate
+        
         aktMag = sum(@view(Layers[1:nodaysvector[j],j])) # this is correct because ea is a nonintegrated with a continuum variable as opposed to discharge
-        differ = aktMag - redea 
-        # println(aktMag)
-        # println(differ)
-        # println(newLayer)
-        # println(total_layers_last)
-        # println(sum(Layers))
 
+        (aktMag == 0) && continue # skip to next layer if it is empty
+        
+        differ = aktMag - redea 
   
         if differ > 0 # the Layer has more water than is to be evaporated > ea_S                
             ea_excess = 0.0
-            evapUH = redea .* @view(layerUH[1:nodaysvector[j],j])
-
-            @views tull = findall(Layers[1:nodaysvector[j],j] .< evapUH[1:nodaysvector[j]])
-            if length(tull) > 0
-              x = tull    # locate which boxes have not enough water to evaporate
-              ea_excess = sum(@view(evapUH[x])) # the amount which is not evaporated
-              evapUH[x] .= 0.0            # the identified boxes have zero instead of negative values
-            end   
-            Layers[1:nodaysvector[j],j] .-= @view(evapUH[1:nodaysvector[j]])
-
-            avvik = round(aktMag - sum(@view(Layers[1:nodaysvector[j],j])) - redea + ea_excess; digits= 8)
-            if avvik != 0
+            tot_layers = 0.0
+            @simd for i in 1:nodaysvector[j]
+              evapUH = redea * layerUH[i,j]
+              if Layers[i,j] < evapUH
+                  ea_excess += evapUH
+              else
+                  Layers[i,j] -= evapUH
+              end
+              tot_layers += Layers[i,j]
+            end
+            avvik = aktMag - tot_layers - redea + ea_excess
+            if abs(avvik) > 1e-8
               println("Hei, feil i fordampning", avvik)
             end  
             redea = 0.0
         else
           Layers[1:nodaysvector[j],j] .= 0.
-          if j == NoL 
-              redea = 0.0
-          else
-              redea -= aktMag                    
-          end
-          #println(newLayer)
+          redea -= aktMag 
         end
-        #println(total_layers_last)       
-        #println(sum(Layers))
-        #println(total_layers_last)    
-      end                             # sumLayers ==0      
-    end                   # redea > 0.0 
-  end                          # for loop
+  end
    
   total_layers_current = sum(Layers)
   ea = total_layers_last - total_layers_current
    
-  if ea > 0
-     if round(total_layers_last - (ea + total_layers_current); digits=8) != 0
-       println("Layers ut not OK")
-     end
-  end
-#println(ea)
-#println(total_layers_last)
-#println(sum(Layers))                                   
+#  if (ea > 0) && abs(total_layers_last - (ea + total_layers_current)) > 1e-8 # COMMENTED OUT AS IT IS A CIRCULAR CHECK
+#      println("Layers ut not OK")
+#  end
+
   return ea
 end
