98ccb1921992b468ff0f891bd5d78ba46c93dc03
Emiliano Gelati <28786167+emiliano-gelati@users.noreply.github.com>
2026-07-15 16:27:20 +0200

    regression test data for 8 catchments (based on 154a15e)

--- test/create_tests.jl
+++ test/create_tests.jl
@@ -1,12 +1,13 @@
 using ProgressMeter
 include(joinpath(@__DIR__, "regression_testing.jl"))
 
-# julia --project=.. --threads 10 create_tests.jl ../calibration/settings/calibration_251120.toml 2.11 247.3
+# julia --project=.. --threads 15 create_tests.jl ../calibration/settings/calibration_251120.toml data/catchments.txt
 
 # Shell arguments
-@assert length(ARGS) > 1
-path_settings = ARGS[1]
-catchments = ARGS[2:end]
+@assert length(ARGS) == 2
+path_settings, path_catchments = ARGS
+# Catchment lists
+catchments = readCatchmentList(path_catchments)
 # Create tests
 println("Create test files for $(length(catchments)) catchments:")
 @showprogress Threads.@threads for id in catchments
